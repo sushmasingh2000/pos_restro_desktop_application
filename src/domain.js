2661971b0b4cb7e3455d5f925292a98702cb81a3
sushmasingh2000 <sushmasingh16166@gmail.com>
2026-09-15 10:52:18 +0530

    port

--- src/domain.js
+++ src/domain.js
@@ -1,5 +1,5 @@
 export const domain = 'https://platterway.com';
-// export const domain = 'http://localhost:9047';
+// export const domain = 'http://localhost:9036';
 export const frontend = domain;
 export const rupees = "₹";
 
